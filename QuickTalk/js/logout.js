4d3f8e8ea3e7d7b3cfb3cc782b9469b6bb208aa0
JoseRdz9 <joserodriiguez500@gmail.com>
2024-04-24 17:34:25 -0600

    delete

--- QuickTalk/js/logout.js
+++
@@ -1,7 +0,0 @@
-function cerrarSesion(uniqueId) {
-    if (uniqueId) {
-      window.location.href = 'php/logout.php?logout_id=' + uniqueId;
-    } else {
-      console.error('ID de usuario no válido');
-    }
-  }
